fa567c80d17c1207a67239615132ab72005e1b12
steve mutuvi <stevemutuvi@gmail.com>
2020-06-14 13:33:01 +0300

    data

--- train_vgcn_bert.py
+++ train_vgcn_bert.py
@@ -494,11 +494,11 @@ testy = pd.DataFrame(testy)
 y_pred = pd.DataFrame(y_pred)
 y_pred_proba = pd.DataFrame(y_pred_proba)
 
-print(testy.head())
+# print(testy.head())
 df_concat_classifier=pd.concat([testy, y_pred, y_pred_proba], axis=1, sort=False, ignore_index=True)
 print(df_concat_classifier.head())
-# df_concat_classifier.columns = ['true_labels','predicted_labels','pred_proba']
-# # print('-'*20, "after concatenation", '-'*20)
-# # print(df_concat_classifier.head())
+df_concat_classifier.columns = ['id','true_labels','predicted_labels','pred_proba']
+print('-'*20, "after concatenation", '-'*20)
+print(df_concat_classifier.head())
 
 # df_concat_classifier.to_csv('vgcn_bert.csv', sep='\t',index=False)
