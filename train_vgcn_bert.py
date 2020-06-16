c59f87357e585aef2245f23539c26c0f97f167ca
steve mutuvi <stevemutuvi@gmail.com>
2020-06-16 12:03:09 +0300

    roc

--- train_vgcn_bert.py
+++ train_vgcn_bert.py
@@ -86,7 +86,7 @@ l2_decay=args.l2
 dataset_list={'sst', 'cola'}
 # hate: 10k, mr: 6753, sst: 7792, r8: 5211
 
-total_train_epochs = 1
+total_train_epochs = 4
 dropout_rate = 0.2  #0.5 # Dropout rate (1 - keep probability).
 if cfg_ds=='sst':
     batch_size = 8 #12   
@@ -518,4 +518,5 @@ df_concat_classifier.columns = ['true_labels','predicted_labels','pred_proba']
 print('-'*20, "after concatenation", '-'*20)
 print(df_concat_classifier.head())
 
+df_concat_classifier.to_csv('vgcn_bert.csv', sep='\t', index=False)
 df_concat_classifier.to_csv('/kaggle/working/vgcn_bert.csv', sep='\t', index=False)
